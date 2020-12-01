329e30223d87a99aded27876bad077eac62deaae
吴智杰 <987165362@qq.com>
2020-12-01 14:37:29 +0800

    v1.3

---
+++ src/views/DataCount.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="dataCount">
+    <h1 class="exampleCountTitle">总测试步骤</h1>
+    <a-table></a-table>
+    <h1>性能</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataCount'
+}
+</script>
+
+<style scoped>
+
+</style>
